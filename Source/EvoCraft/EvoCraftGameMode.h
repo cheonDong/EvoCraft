a796f70ddd8d725b6e7799978650c7a81d5adbf2
o_odong <skai0907@gmail.com>
2023-11-21 14:19:15 +0900

    first Commit

---
+++ Source/EvoCraft/EvoCraftGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EvoCraftGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEvoCraftGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEvoCraftGameMode();
+};
+
+
+
